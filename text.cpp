045fda97db87c40e3b568ac783f27fdc9cd31b1c
unknown <trantuan9122003@gmail.com>
2024-11-16 08:30:45 +0700

    thay doi nhanh le

--- text.cpp
+++ text.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void main() {
-//-----------------------
-	int a;
-	cout << "nhap vao so nguyen a: ";
-	cin >> a;
-	cout << "\n Gia tri so a=[" << a << "]";
-//--------------------------
-    if(a%2!= 0){
-        cout<<"\nSo ["<<a <<"] la so le.";
+void main()
+{
+    //-----------------------------------------
+    int a;
+    cout << "nhap vao so nguyen a: ";
+    cin >> a;
+    cout << "\n Gia tri so a=[" << a << "]";
+    //-------------------------------------
+    if (a % 2 != 0)
+    {
+        cout << "\nSo [" << a << "] la so le;";
     }
 }
