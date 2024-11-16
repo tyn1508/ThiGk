573d5948aed36fc859b0b1101e877cd69bd15330
unknown <trantuan9122003@gmail.com>
2024-11-16 08:22:13 +0700

    chuong trinh le

--- text.cpp
+++ text.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 void main() {
+//-----------------------
 	int a;
 	cout << "nhap vao so nguyen a: ";
 	cin >> a;
 	cout << "\n Gia tri so a=[" << a << "]";
+//--------------------------
+    if(a%2!= 0){
+        cout<<"\nSo ["<<a <<"] la so le.";
+    }
 }
